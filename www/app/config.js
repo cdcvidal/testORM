223d59ed00d742384d4a528bab52777d181154de
cdcvidal <celine_vidal@natural-solutions.eu>
2015-07-02 11:44:26 +0200

    Add config files

---
+++ www/app/config.js
@@ -0,0 +1,6 @@
+define({
+root: window.location.pathname.replace(/\/(?:index.html)?$/, ''),
+coreUrl: 'http://127.0.0.1/',
+dateLabel: 'jj/mm/aaaa hh:mm:ss',
+dateFormats: ['DD/MM/YYYY', 'DD/MM/YYYY HH:mm:ss'],
+});
